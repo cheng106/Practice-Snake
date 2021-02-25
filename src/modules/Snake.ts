0869c9ab1e4c3f3db0beeb90d33e90bdf3089927
ChengYu <cheng106.andy@gmail.com>
2021-02-25 14:16:28 +0800

    feat: Snake grows out of body

--- src/modules/Snake.ts
+++ src/modules/Snake.ts
@@ -30,6 +30,10 @@ export default class Snake {
         if (value < 0 || value > 290) {
             throw new Error('蛇撞牆必須死')
         }
+
+        // 移動身體
+        this.moveBody()
+
         this.head.style.left = value + 'px'
     }
 
@@ -40,6 +44,10 @@ export default class Snake {
         if (value < 0 || value > 290) {
             throw new Error('蛇撞牆必須死')
         }
+
+        // 移動身體
+        this.moveBody()
+
         this.head.style.top = value + 'px'
     }
 
@@ -47,5 +55,26 @@ export default class Snake {
     addBody() {
         //向element中增加一個div
         this.element.insertAdjacentHTML('beforeend', '<div></div>')
+    }
+
+    // 蛇身體移動
+    moveBody() {
+        // 將最後一節的身體位置往前一節的身體位置修改
+        /*
+        * 4節 = 3節
+        * 3節 = 2節
+        * 2節 = 蛇頭
+        * */
+        // 取所有蛇身體
+        for (let i = this.bodies.length - 1; i > 0; i--) {
+            // 取前一節身體位置
+            let x = (this.bodies[i - 1] as HTMLElement).offsetLeft;
+            let y = (this.bodies[i - 1] as HTMLElement).offsetTop;
+
+            // 修改身體位置
+            (this.bodies[i] as HTMLElement).style.left = x + 'px';
+            (this.bodies[i] as HTMLElement).style.top = y + 'px';
+        }
+
     }
 }
