02219e024c83ab27a5e96623ba86724aee6404dd
ChengYu <cheng106.andy@gmail.com>
2021-02-25 14:49:28 +0800

    feat: Check if the snake's body hits the wall or hit itself

--- src/modules/Snake.ts
+++ src/modules/Snake.ts
@@ -31,11 +31,26 @@ export default class Snake {
             throw new Error('蛇撞牆必須死')
         }
 
+        // 修改X時，是修改水平座標，左右移動時不能向左/右調頭(防止蛇穿過自己身體)
+        if (this.bodies[1] && (this.bodies[1] as HTMLElement).offsetLeft === value) {
+            /* 發生調頭讓蛇反向移動 */
+            // value大於X，表示蛇向右走
+            if (value > this.X) {
+                value = this.X - 10
+            } else {
+                // 向左走
+                value = this.X + 10
+            }
+        }
+
+
         // 移動身體
         this.moveBody()
-
         this.head.style.left = value + 'px'
+        // 檢查是否撞到身體
+        this.checkHeadBody()
     }
+
 
     set Y(value: number) {
         if (this.Y === value) {
@@ -45,10 +60,20 @@ export default class Snake {
             throw new Error('蛇撞牆必須死')
         }
 
+        // 修改Y時，是修改垂直座標，上下移動時不能向上/下調頭(防止蛇穿過自己身體)
+        if (this.bodies[1] && (this.bodies[1] as HTMLElement).offsetTop === value) {
+            if (value > this.Y) {
+                value = this.Y - 10
+            } else {
+                value = this.Y + 10
+            }
+        }
+
         // 移動身體
         this.moveBody()
-
         this.head.style.top = value + 'px'
+        // 檢查是否撞到身體
+        this.checkHeadBody()
     }
 
     // 蛇增加身體
@@ -75,6 +100,15 @@ export default class Snake {
             (this.bodies[i] as HTMLElement).style.left = x + 'px';
             (this.bodies[i] as HTMLElement).style.top = y + 'px';
         }
+    }
 
+    checkHeadBody() {
+        // 取所有蛇身體，檢查是否和蛇頭的座標發生重疊
+        for (let i = 1; i < this.bodies.length; i++) {
+            let body = this.bodies[i] as HTMLElement;
+            if (this.X === body.offsetLeft && this.Y === body.offsetTop) {
+                throw new Error('撞到身體被纏死')
+            }
+        }
     }
 }
